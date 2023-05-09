c1732416390a8ff2a94a682519e68585962d3136
Excel951 <abigailexcelsisdeo@gmail.com>
2023-05-09 10:56:37 +0700

    update uts mobile

---
+++ app/src/main/java/com/aedeo/myaccounting21410100045/MainActivity.java
@@ -0,0 +1,215 @@
+package com.aedeo.myaccounting21410100045;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.DatePickerDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Parcelable;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+
+public class MainActivity extends AppCompatActivity {
+
+    DatePickerDialog datePickerDialog;
+    //    =====================================
+//    object transaksi
+    Transaksi transaksi = new Transaksi();
+
+    //    arraylist dari object transaksi
+    ArrayList<Transaksi> listTransaksi = new ArrayList<Transaksi>();
+
+    //    =======================================
+    String valueIDTransaksi = "", valueKeterangan = "";
+    Integer valueDebit = 0, valueKredit = 0, valueLogo = 0;
+    Date valueTanggal = null;
+
+    ListView listViewTransaksi;
+    private Button pilihTanggal, btnLaporanMain, btnTambah;
+    private EditText tanggal, inputId_transaksi, inputViewTanggal_transaksi, inputKet_transaksi, inputDebit_transaksi, inputKredit_transaksi;
+
+    private SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
+
+    private View myCoordinatorLayout;
+
+    private Context notif;
+
+    private AdapterListTransaksi adapterListTransaksi;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//        Deklarasi Element
+//        List View
+        listViewTransaksi = (ListView) findViewById(R.id.listView_Transaksi);
+//        Context
+        notif = getApplicationContext();
+//        EditText
+        tanggal = (EditText) findViewById(R.id.inputViewTanggal_transaksi);
+        inputId_transaksi = (EditText) findViewById(R.id.inputId_transaksi);
+        inputViewTanggal_transaksi = (EditText) findViewById(R.id.inputViewTanggal_transaksi);
+        inputKet_transaksi = (EditText) findViewById(R.id.inputKet_transaksi);
+        inputDebit_transaksi = (EditText) findViewById(R.id.inputDebit_transaksi);
+        inputKredit_transaksi = (EditText) findViewById(R.id.inputKredit_transaksi);
+//        Button
+        pilihTanggal = (Button) findViewById(R.id.btnInputTanggal);
+        btnLaporanMain = (Button) findViewById(R.id.btnLaporan);
+        btnTambah = (Button) findViewById(R.id.btnTambah);
+
+//        ketika klik button pilih tanggal
+        showDateDialog();
+
+//        ketika klik button laporan
+        pindahKeLaporan();
+
+//        ketika klik button tambah
+        tambahTransaksi();
+    }
+
+    private void clearForm() {
+        inputId_transaksi.setText("");
+        inputViewTanggal_transaksi.setText("");
+        inputKet_transaksi.setText("");
+        inputDebit_transaksi.setText("");
+        inputKredit_transaksi.setText("");
+
+        valueIDTransaksi = "";
+        valueKeterangan = "";
+        valueDebit = 0;
+        valueKredit = 0;
+        valueLogo = 0;
+        valueTanggal = null;
+    }
+
+    private void tambahTransaksi() {
+        btnTambah.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("TES!!!", "onClick: TES");
+                getValueTransaksi();
+                if (valueIDTransaksi == "" || valueKeterangan == "" || valueTanggal == null) {
+//                    ketika ada kolom yang masih kosong
+                    String text = "Masih ada kolom yang kosong";
+                    Toast warning = Toast.makeText(notif, text, Toast.LENGTH_SHORT);
+                    warning.show();
+                } else if (valueDebit == valueKredit) {
+//                    ketika debit dan kredit sama besar
+                    String text = "Debit dan Kredit tidak dapat sama besar";
+                    Toast warning = Toast.makeText(notif, text, Toast.LENGTH_SHORT);
+                    warning.show();
+                } else {
+//                ===============================
+//                masukkan transaksi ke dalam object
+                    transaksi = new Transaksi(valueIDTransaksi, valueKeterangan, valueTanggal, valueDebit, valueKredit, valueLogo);
+
+//                tambahkan object kedalam arraylist
+                    listTransaksi.add(transaksi);
+
+                    for (int i = 0; i < listTransaksi.size(); i++) {
+                        Log.d("TAG", "onClick: " + listTransaksi.get(i).getIdTransaksi());
+                    }
+
+//        masukkan ke dalam adapter
+                    adapterListTransaksi = new AdapterListTransaksi(MainActivity.this, listTransaksi);
+//        implementasi list view
+                    listViewTransaksi.setAdapter(adapterListTransaksi);
+
+//                    bersihkan form
+                    clearForm();
+//                ================================
+                }
+            }
+        });
+    }
+
+    private void getValueTransaksi() {
+//        fungsi untuk mengambil value dari inputan user
+//        ambil id transaksi
+        if (!inputId_transaksi.getText().toString().isEmpty()) {
+            valueIDTransaksi = inputId_transaksi.getText().toString();
+        }
+//        ambil tanggal transaksi
+        try {
+            if (!inputViewTanggal_transaksi.getText().toString().isEmpty()) {
+                valueTanggal = sdf.parse(inputViewTanggal_transaksi.getText().toString());
+            }
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
+//        ambil keterangan
+        if (!inputKet_transaksi.getText().toString().isEmpty()) {
+            valueKeterangan = String.valueOf(inputKet_transaksi.getText());
+        }
+//        ambil debit dan kredit
+        if (!inputKredit_transaksi.getText().toString().isEmpty()) {
+            valueKredit = Integer.valueOf(inputKredit_transaksi.getText().toString());
+        }
+        if (!inputDebit_transaksi.getText().toString().isEmpty()) {
+            valueDebit = Integer.valueOf(inputDebit_transaksi.getText().toString());
+        }
+//        menentukan logo
+        if (valueDebit > valueKredit) {
+            valueLogo = R.drawable.dlogo;
+            valueKredit = 0;
+        } else if (valueKredit > valueDebit) {
+            valueLogo = R.drawable.klogo;
+            valueDebit = 0;
+        }
+    }
+
+    private void pindahKeLaporan() {
+        btnLaporanMain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//        declare pindah rekap
+                Intent pindahRekap = new Intent(MainActivity.this, RekapTransaksi.class);
+//                Intent pindahRekap = new Intent(, RekapTransaksi.class);
+
+//        kirim data
+//                pindahRekap.putParcelableArrayListExtra("ListTransaksi", );
+                pindahRekap.putExtra("list", listTransaksi);
+
+//        mulai activity
+                startActivity(pindahRekap);
+            }
+        });
+    }
+
+    private void showDateDialog() {
+        pilihTanggal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                deklarasi kalender dan ambil tahun, bulan, dan tanggal dari dialog
+                final Calendar cal = Calendar.getInstance();
+                int currentYear = cal.get(Calendar.YEAR);
+                int currentMonth = cal.get(Calendar.MONTH);
+                int currentDate = cal.get(Calendar.DAY_OF_MONTH);
+
+//                date picker dialog
+                datePickerDialog = new DatePickerDialog(MainActivity.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                        tanggal.setText(day + "-" + (month + 1) + "-" + year);
+                    }
+                }, currentYear, currentMonth, currentDate);
+                datePickerDialog.show();
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/aedeo/myaccounting21410100045/RekapTransaksi.java
@@ -0,0 +1,82 @@
+package com.aedeo.myaccounting21410100045;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class RekapTransaksi extends AppCompatActivity {
+
+
+    //    Bundle bundle = getIntent().getExtras();
+    ArrayList<Transaksi> listTransaksiClone = new ArrayList<>();
+    Integer totalKredit = 0, totalDebit = 0, totalSeluruh = 0;
+    private Button btnLaporanRekap;
+    private TextView rekapPemasukan, rekapPengeluaran, rekapSaldo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rekap_transaksi);
+
+//        deklarasi element
+//        intent
+        Intent intent = getIntent();
+//        button
+        btnLaporanRekap = (Button) findViewById(R.id.btnLaporanRekap);
+//        textview
+        rekapPemasukan = (TextView) findViewById(R.id.rekapPemasukan);
+        rekapPengeluaran = (TextView) findViewById(R.id.rekapPengeluaran);
+        rekapSaldo = (TextView) findViewById(R.id.rekapSaldo);
+
+        ArrayList<Transaksi> listTransaksi = (ArrayList<Transaksi>) intent.getSerializableExtra("list");
+        listTransaksiClone = (ArrayList<Transaksi>) listTransaksi.clone();
+//        pindah ke main
+        pindahKeMain();
+
+//        hitung total dari rekap
+        hitungTotalTrx();
+
+//        set value rekap
+        setRekap();
+    }
+
+    private void setRekap() {
+        rekapPemasukan.setText("Rp. "+totalDebit.toString());
+        rekapPengeluaran.setText("Rp. "+totalKredit.toString());
+        rekapSaldo.setText("Rp. "+totalSeluruh.toString());
+    }
+
+    private void hitungTotalTrx() {
+        for (int i = 0; i < listTransaksiClone.size(); i++) {
+            totalKredit += listTransaksiClone.get(i).getKredit();
+            Log.d("TAG", "hitungTotalTrx: " + totalKredit);
+            totalDebit += listTransaksiClone.get(i).getDebit();
+            Log.d("TAG", "hitungTotalTrx: " + totalDebit);
+            totalSeluruh = totalDebit - totalKredit;
+            Log.d("TAG", "hitungTotalTrx: " + totalKredit);
+        }
+    }
+
+    private void pindahKeMain() {
+        btnLaporanRekap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+//                deklarasi intent
+//                Intent pindahMain = new Intent(RekapTransaksi.this, MainActivity.class);
+//
+//                pindahMain.putExtra("list", listTransaksiClone);
+
+//                jalankan activity main
+//                startActivity(pindahMain);
+            }
+        });
+    }
+}
